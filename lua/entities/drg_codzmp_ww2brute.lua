395c9faee48a555547cd092b845a9ef5db8362eb
Mayhem <VpSnipes@users.noreply.github.com>
2020-11-29 23:05:33 -0500

    Panzermorder
    >New Model/Materials
    >New Event types for tracking
    >Removed useless sound that had Klaus's voice in it (Not used, only used as a permutation to another)
    >Adjuested code to fit new model
    >Code adjustments/improvements for walking/running attacks
    >Added option for heavy and light attacks

--- lua/entities/drg_codzmp_ww2brute.lua
+++ lua/entities/drg_codzmp_ww2brute.lua
@@ -4,7 +4,7 @@ ENT.Base = "drgbase_nextbot" -- DO NOT TOUCH (obviously)
 -- Misc --
 ENT.PrintName = "Panzermoerder"
 ENT.Category = "CoDZ Megapack: WW2"
-ENT.Models = {"models/roach/codz_megapack/ww2/brute.mdl"}
+ENT.Models = {"models/roach/codz_megapack/ww2/panzermorder.mdl"}
 ENT.BloodColor = BLOOD_COLOR_RED
 
 -- Relationships --
@@ -22,33 +22,45 @@ ENT.AvoidEnemyRange = 0
 ENT.FollowPlayers = true
 
 -- Detection --
-ENT.EyeBone = "j_head"
-ENT.EyeOffset = Vector(7.5, 0, 5)
+ENT.EyeBone = "tongue"
+ENT.EyeOffset = Vector(0, 0, 5)
 ENT.EyeAngle = Angle(0, 0, 0)
 
 -- Possession --
 ENT.PossessionEnabled = true
 ENT.PossessionViews = {
   {
-    offset = Vector(0, 30, 20),
+    offset = Vector(-20, 90, 80),
     distance = 100
   },
   {
-    offset = Vector(1.5, 1, -3),
+    offset = Vector(0, 0, 0),
     distance = 0,
     eyepos = true
   }
 }
 ENT.PossessionBinds = {
-	[IN_ATTACK] = {{coroutine = true,onkeydown = function(self)
+	[IN_ATTACK] = {{coroutine = true,onkeydown = function(self) -- Light Attacks
+		if not self:IsMoving() and not self:IsRunning() then
+			self:PlaySequenceAndMove("s2_zom_brt_stand_atk_v1",1,self.PossessionFaceForward)
+			
+		elseif self:IsRunning() then
+			self:PlaySequenceAndMove("s2_zom_brt_run_attack_v1",1,self.PossessionFaceForward)
+			
+		elseif self:IsMoving() and not self:IsRunning() then
+			self:PlaySequenceAndMove("s2_zom_brt_walk_attack_v"..math.random(2),1,self.PossessionFaceForward)
+		end
+	end}},
+	[IN_ATTACK2] = {{coroutine = true,onkeydown = function(self) -- Heavy Attacks
 		if not self:IsRunning() then
-			self:PlaySequenceAndMove("att"..math.random(5),1,self.PossessionFaceForward)
-		else
-			self:PlaySequenceAndMove("att_run",1,self.PossessionFaceForward)
+			self:PlaySequenceAndMove("s2_zom_brt_walk_attack_ground_v"..math.random(2),1,self.PossessionFaceForward)
 		end
 	end}},
 	[IN_JUMP] = {{coroutine = true,onkeydown = function(self)
-		self:PlaySequenceAndMoveAbsolute("traverse",1,self.PossessionFaceForward)
+		self:PlaySequenceAndMoveAbsolute("s2_zom_brt_walk_trav_exit_v1",1,self.PossessionFaceForward)
+	end}},
+	[IN_RELOAD] = {{coroutine = true,onkeydown = function(self)
+		self:PlaySequenceAndMoveAbsolute("s2_zom_brt_roar",1,self.PossessionFaceForward)
 	end}},
 	[IN_ATTACK3] = {{coroutine = true,onkeydown = function(self)
 		self:Suicide()
@@ -68,22 +80,22 @@ game.AddParticles("particles/blackops3zombies_fx.pcf")
 if SERVER then
 
 function ENT:OnSpawn()
-	self:PlaySequenceAndMove("roar")
+	self:PlaySequenceAndMove("s2_zom_brt_roar")
 end
 
 function ENT:OnLandOnGround()
 	if self:IsDead() then return end
-	self:CICO(function()self:PlaySequenceAndMove("land")end)
+	self:CICO(function()self:PlaySequenceAndMove("s2_zom_brt_land")end)
 end
 
 function ENT:CustomInitialize()
 	self:SetDefaultRelationship(D_HT)
 	self:AddPlayersRelationship(D_HT,2)
 	
-	self.WalkAnimation = "walk"
-	self.RunAnimation = "run"
-	self.IdleAnimation = "idle"
-	self.JumpAnimation = "glide"
+	self.WalkAnimation = "s2_zom_brt_walk"
+	self.RunAnimation = "s2_zom_brt_run"
+	self.IdleAnimation = "s2_zom_brt_idle"
+	self.JumpAnimation = "s2_zom_brt_glide"
 	
 	self.SOUND_EVTTABLE = {
 		["s2_zom_brt_walk_trav_exit_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_walk_trav_exit_v1_0", 3},
@@ -92,7 +104,7 @@ function ENT:CustomInitialize()
 		["s2_zom_brt_stun_up_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_stun_up_v1_0", 3},
 		["s2_zom_brt_stun_loop_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_stun_loop_v1_0", 3},
 		["s2_zom_brt_stun_down_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_stun_down_v1_0", 3},
-		["s2_zom_brt_walk_attack_v2"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_walk_attack_v2_0", 4},
+		["s2_zom_brt_walk_attack_v2"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_walk_attack_v2_0", 3},
 		["s2_zom_brt_walk_attack_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_walk_attack_v1_0", 3},
 		["s2_zom_brt_walk_attack_ground_v2"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_walk_attack_ground_v2_0", 3},
 		["s2_zom_brt_walk_attack_ground_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_walk_attack_ground_v1_0", 3},
@@ -101,7 +113,7 @@ function ENT:CustomInitialize()
 		["s2_zom_brt_run_attack_v1"] = {"codz_megapack/ww2/brute/vox/s2_zom_brt_run_attack_v1_0", 5}
 	}
 	-- self:SetModelScale(1.5,0)
-	self:Timer(0.1,self.SetCollisionBounds, Vector(-100,-100,0), Vector(100, 100, 220))
+	self:Timer(0.1,self.SetCollisionBounds, Vector(-80,-80,0), Vector(80, 80, 220))
 	self:Timer(0.2,self.PhysicsInitShadow)
 end
 
@@ -118,7 +130,8 @@ function ENT:HandleAnimEvent(a,b,c,d,e)
 				self:EmitSound("codz_megapack/ww2/fol/zmb_follower_mace_gore_0"..math.random(5)..".wav",100,math.random(95,105))
 			end
 		end)
-	elseif e == "step" then 
+	
+	elseif e == "footstep_walk_right_brute" or e == "footstep_walk_left_brute" then 
 		if self:IsRunning() then
 			self:EmitSound("codz_megapack/ww2/brute/zmb_fs_run_brute_default2_0"..math.random(9)..".wav",80)
 			util.ScreenShake(self:GetPos(),100000,500000,0.2,300)
@@ -126,8 +139,9 @@ function ENT:HandleAnimEvent(a,b,c,d,e)
 			self:EmitSound("codz_megapack/ww2/brute/zmb_fs_walk_brute_default2_0"..math.random(7)..".wav",80)
 			util.ScreenShake(self:GetPos(),100000,500000,0.2,150)
 		end
+	
 	-- elseif e == "start_ragdoll" then self:BecomeRagdoll(DamageInfo())
-	elseif e == "bodyfall" then 
+	elseif e == "bodyfall_large" then 
 		self:EmitSound("codz_megapack/ww2/brute/zmb_fs_walk_brute_default2_0"..math.random(8,9)..".wav",511,math.random(80,100))
 		for i=1,3 do ParticleEffect("bo3_panzer_landing",self:LocalToWorld(Vector(math.random(-20,20),0,0)), Angle(0,0,0), nil) end
 		util.ScreenShake(self:GetPos(),100000,500000,1,512)
@@ -142,6 +156,11 @@ function ENT:HandleAnimEvent(a,b,c,d,e)
 		ParticleEffect("bo3_panzer_landing",self:LocalToWorld(Vector(math.random(-20,20),-30,0)), Angle(0,0,0), nil)
 	end
 	
+	if string.StartWith(e, "TurnSpeed_") then
+		local nb = tonumber(string.sub(e, 11))
+		self:SetMaxYawRate(nb)
+	end
+	
 	local evt = string.Explode(" ", e, false)
 	if evt[1] == "ps" then
 		local snd = self.SOUND_EVTTABLE[evt[2]]
@@ -152,9 +171,19 @@ end
 function ENT:OnMeleeAttack(enemy)
 	if self.Charging then
 		self.Charging = false
-		self:PlaySequenceAndMove("att_run",1,self.FaceEnemy)
-	else
-		self:PlaySequenceAndMove("att"..math.random(5),1,self.FaceEnemy)
+		self:PlaySequenceAndMove("s2_zom_brt_run_attack_v1",1,self.FaceEnemy)
+	end
+	
+	if not self:IsMoving() and not self:IsRunning() then
+		self:PlaySequenceAndMove("s2_zom_brt_stand_atk_v1",1,self.FaceEnemy)
+
+	elseif self:IsMoving() and not self:IsRunning() then
+		local atk = math.random(1,2)
+		if atk == 1 then
+			self:PlaySequenceAndMove("s2_zom_brt_walk_attack_ground_v"..math.random(2),1,self.FaceEnemy)
+		else
+			self:PlaySequenceAndMove("s2_zom_brt_walk_attack_v"..math.random(2),1,self.FaceEnemy)
+		end
 	end
 end
 function ENT:OnRangeAttack(enemy)
@@ -168,7 +197,7 @@ function ENT:OnReachedPatrol(pos)self:Wait(math.random(3, 7))end
 function ENT:OnNewEnemy(enemy)
 	self:CallInCoroutine(function(self,delay)
 		if delay > 0.1 then return end
-		self:PlaySequenceAndMove("roar",1,self.FaceEnemy)
+		self:PlaySequenceAndMove("s2_zom_brt_roar",1,self.FaceEnemy)
 	end)
 end
 
@@ -182,7 +211,7 @@ function ENT:OnTakeDamage(dmg,hg)
 		self.Flinching = true
 		self.Charging = false
 		self:CICO(function()
-			self:PlaySequenceAndMove("att_stagger")
+			self:PlaySequenceAndMove("s2_zom_brt_stand_atk_lurch_v1")
 			self.Flinching = false
 		end)
 	end
@@ -191,15 +220,15 @@ end
 function ENT:OnFatalDamage()return true end
 function ENT:OnDowned(dmg)
 	self.Charging = false
-	self:PlaySequenceAndMove("down_1")
+	self:PlaySequenceAndMove("s2_zom_brt_stun_down_v1")
 	
 	local duration = 30
 	self:SetHealthRegen(math.ceil((self:GetMaxHealth()-self:Health())/duration))
 	while self:Health() < self:GetMaxHealth() do
-		self:PlaySequenceAndMove("down_2")
+		self:PlaySequenceAndMove("s2_zom_brt_stun_loop_v1")
 		self:YieldCoroutine(false)
 	end
-	self:PlaySequenceAndMove("down_3")
+	self:PlaySequenceAndMove("s2_zom_brt_stun_up_v1")
 	self:SetHealthRegen(0)
 end
 
